2776ceb117a550a492d9ea351ecb1040aa79f3dc
Carlos López <calopezs1110@gmail.com>
2025-03-08 01:16:19 -0500

    fix: Quit zone.js

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,16 @@
-import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  isMainModule,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
-import { AngularAppEngine, createRequestHandler } from '@angular/ssr'
+import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
-
+import { isMainModule } from '@angular/ssr/node';
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
-const angularAppEngine = new AngularAppEngine()
-import { getContext } from '@netlify/angular-runtime/context'
 
+const angularAppEngine = new AngularAppEngine();
+import { getContext } from '@netlify/angular-runtime/context';
 
-export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-  const context = getContext()
-
-  // Example API endpoints can be defined here.
-  // Uncomment and define endpoints as necessary.
-  // const pathname = new URL(request.url).pathname;
-  // if (pathname === '/api/hello') {
-  //   return Response.json({ message: 'Hello from the API' });
-  // }
-
-  const result = await angularAppEngine.handle(request, context)
-  return result || new Response('Not found', { status: 404 })
-}
 
 /**
  * Example Express Rest API endpoints can be defined here.
@@ -52,20 +32,8 @@ app.use(
     maxAge: '1y',
     index: false,
     redirect: false,
-  }),
+  })
 );
-
-/**
- * Handle all other requests by rendering the Angular application.
- */
-app.use('/**', (req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
-});
 
 /**
  * Start the server if this module is the main entry point.
@@ -78,7 +46,19 @@ if (isMainModule(import.meta.url)) {
   });
 }
 
+
+export async function netlifyAppEngineHandler(
+  request: Request
+): Promise<Response> {
+  const context = getContext();
+
+  const result = await angularAppEngine.handle(request, context);
+  return result || new Response('Not found', { status: 404 });
+}
+
+
+
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
-export const reqHandler = createNodeRequestHandler(app);
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
