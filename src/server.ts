ed166f544d993a5e0f92cf397110f066684f48c7
Carlos López <calopezs1110@gmail.com>
2025-03-08 00:30:51 -0500

    fix: Quit zone.js

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,37 @@
 import {
-  AngularNodeAppEngine,
+
+  CommonEngine,
   createNodeRequestHandler,
   isMainModule,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
-import express from 'express';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
 
+} from '@angular/ssr/node';
+import bootstrap from './main.server';
+import { APP_BASE_HREF } from '@angular/common';
+import express from 'express';
+import { dirname, join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { render } from '@netlify/angular-runtime/common-engine'
 
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
+const indexHtml = join(serverDistFolder, 'index.server.html');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine()
+const commonEngine = new CommonEngine();
+
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  return await render(commonEngine)
+}
+
+
 
 /**
  * Example Express Rest API endpoints can be defined here.
@@ -31,24 +48,32 @@ const angularApp = new AngularNodeAppEngine()
 /**
  * Serve static files from /browser
  */
-app.use(
+app.get(
+  '**',
   express.static(browserDistFolder, {
     maxAge: '1y',
-    index: false,
-    redirect: false,
+    index: 'index.html'
   }),
 );
 
 /**
  * Handle all other requests by rendering the Angular application.
  */
-app.use('/**', (req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
+app.get('**', (req, res, next) => {
+  const { protocol, originalUrl, baseUrl, headers } = req;
+
+  console.log(baseUrl);
+
+  commonEngine
+    .render({
+      bootstrap,
+      documentFilePath: indexHtml,
+      url: `${protocol}://${headers.host}${originalUrl}`,
+      publicPath: browserDistFolder,
+      providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+    })
+    .then((html) => res.send(html))
+    .catch((err) => next(err));
 });
 
 /**
