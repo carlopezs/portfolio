e200d815da9d144f4cbef73d54f0248bd06a7650
Carlos López <calopezs1110@gmail.com>
2025-04-07 23:13:39 -0500

    subida netlify

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,21 @@ import bootstrap from './main.server';
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 const indexHtml = join(serverDistFolder, 'index.server.html');
+import { render } from '@netlify/angular-runtime/common-engine'
 
 const app = express();
 const commonEngine = new CommonEngine();
+
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  return await render(commonEngine)
+}
 
 /**
  * Example Express Rest API endpoints can be defined here.
