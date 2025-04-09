18bf39ac06c1e4e9a35ac011c73b8ac0ebe698c2
Carlos López <calopezs1110@gmail.com>
2025-04-08 20:33:09 -0500

    subida netlify

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -18,7 +18,7 @@ import { ProjectsMenuLaptopComponent } from '@components/projects/projects-menu-
 import { ToolChipComponent } from '@components/projects/tool-chip/tool-chip.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { take } from 'rxjs';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-projects',
@@ -36,6 +36,7 @@ export default class ProjectsComponent implements OnInit {
   private projectService = inject(ProjectsService);
   private activatedRoute = inject(ActivatedRoute);
   private title = inject(Title);
+  private meta = inject(Meta);
 
   public projects: WritableSignal<Project[]> = linkedSignal(() =>
     this.projectService.projects()
@@ -48,14 +49,22 @@ export default class ProjectsComponent implements OnInit {
   ngOnInit(): void {
     this.projectService.loadProjects();
     this.activatedRoute.params.subscribe((value) => {
-
       if (value['title'] == 'all') {
         this.title.setTitle(this.projectToShow().title);
         return;
       }
-      const title = this.projectService.chooseProjectToShowByTitle(value['title']);
-      this.title.setTitle(title?? 'Projects');
-
+      const project = this.projectService.chooseProjectToShowByTitle(
+        value['title']
+      );
+      if (!project) return;
+      this.title.setTitle(project.title ?? 'Projects');
+      this.meta.updateTag({
+        name: 'description',
+        content: project.description!,
+      });
+      this.meta.updateTag({ name: 'og:title', content: project.title! });
+      this.meta.updateTag({name:'og:descrciption', content:project.description})
+      this.meta.updateTag({name:'og:image', content:project.image})
     });
   }
 }
