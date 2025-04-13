0fc4cbc1f1bb3375eafa9899fccf19e4d8ce2731
Carlos López <calopezs1110@gmail.com>
2025-04-13 16:31:04 -0500

    subida netlify

--- src/app/pages/certificates/certificates.component.ts
+++ src/app/pages/certificates/certificates.component.ts
@@ -1,7 +1,14 @@
 import { Certificate } from '@models/certificate.model';
 import { CertificatesCardComponent } from '@components/certificates/certificates-card/certificates-card.component';
 import { CertificatesService } from 'app/services/certificates.service';
-import { ChangeDetectionStrategy, Component, computed, inject, OnInit, Signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  OnInit,
+  Signal,
+} from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
@@ -13,13 +20,12 @@ import { Meta, Title } from '@angular/platform-browser';
   selector: 'app-certificates',
   imports: [CertificatesCardComponent, MatIcon, ReactiveFormsModule],
   templateUrl: './certificates.component.html',
-  changeDetection:ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class CertificatesComponent implements OnInit {
   private readonly certificatesService = inject(CertificatesService);
   private title = inject(Title);
   private meta = inject(Meta);
-
 
   public allCertificates = computed(() =>
     this.certificatesService.certificates()
@@ -44,23 +50,36 @@ export default class CertificatesComponent implements OnInit {
     )
   );
 
-  constructor(){
-
-  }
-
-
+  constructor() {}
 
   ngOnInit(): void {
     this.certificatesService.loadCertificates();
     this.searchControl.setValue('');
 
     this.title.setTitle('Certificates');
+
     this.meta.updateTag({
       name: 'description',
-      content: 'I have earned multiple certifications that showcase my expertise in both frontend and backend development. ',
+      content:
+        'I have earned multiple certifications that showcase my expertise in both frontend and backend development. ',
     });
-    this.meta.updateTag({ name: 'og:title', content: 'Certificates'});
 
+    this.meta.updateTag({ name: 'og:title', content: 'Certificates' });
+
+    this.meta.updateTag({
+      name: 'og:description',
+      content:
+        'I have earned multiple certifications that showcase my expertise in both frontend and backend development.',
+    });
+
+    const lastImage = this.allCertificates()[0].image.substring(7);
+
+    console.log(lastImage);
+    this.meta.updateTag({
+      name: 'og:image',
+      content:
+        `https://portfolioclopez.netlify.app/images/${lastImage}`,
+    });
 
 
   }
